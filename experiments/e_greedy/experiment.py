682134a06dca5c07dbf43ae5525e1fb9b82ff4c4
Bart Keulen <bart_keulen@hotmail.com>
2017-09-12 11:30:21 -0500

    Seperated experiment from plot for e-greedy.

--- experiments/e_greedy/experiment.py
+++ experiments/e_greedy/experiment.py
@@ -1,15 +1,9 @@
-import os
-
 import numpy as np
-import matplotlib.pyplot as plt
 
-from algorithms.tdlearning import TDLearning
 from algorithms.qlearning import QLearning
-from algorithms.sarsa import SARSA, SARSALamba
-from algorithms.smartstart import SmartStart
-from environments.gridworld import GridWorldVisualizer, GridWorld
+from algorithms.tdlearning import TDLearning
+from environments.gridworld import GridWorld
 from utilities.experimenter import run_experiment
-from utilities.plot import plot_mean_std, plot_mean
 from utilities.utilities import get_data_directory
 
 directory = get_data_directory(__file__)
@@ -18,7 +12,7 @@ maze_type = [GridWorld.EASY, GridWorld.MEDIUM]
 alphas = [0.01, 0.05, 0.1, 0.25, 0.5, 1.]
 epsilons = [0.01, 0.05, 0.1, 0.25]
 gammas = [0.5, 0.9, 0.95, 0.99, 0.999]
-num_exp = 10
+num_exp = 1
 
 
 def task(params):
@@ -60,35 +54,6 @@ param_grid = {'task': task,
               'gamma': gammas,
               'num_exp': num_exp}
 
-# run_experiment(param_grid)
 
-mazes = ["GridWorldEasy", "GridWorldMedium"]
-algo = "QLearning"
-
-for maze in mazes:
-    files = []
-    legend = []
-    for alpha in alphas:
-        legend.append(r'alpha = %.2f' % alpha)
-        files.append(os.path.join(directory, "%s_%s_alpha=%.2f_gamma=0.990_epsilon=0.05" % (algo, maze, alpha)))
-    title = "%s %s alpha" % (algo, maze)
-    plot_mean(files, num_exp, title, legend)
-
-    files = []
-    legend = []
-    for gamma in gammas:
-        legend.append(r'gamma = %.3f' % gamma)
-        files.append(os.path.join(directory, "%s_%s_alpha=0.10_gamma=%.3f_epsilon=0.05" % (algo, maze, gamma)))
-    title = "%s %s gamma" % (algo, maze)
-    plot_mean(files, num_exp, title, legend)
-
-    files = []
-    legend = []
-    for epsilon in epsilons:
-        legend.append(r'$\epsilon$ = %.2f' % epsilon)
-        files.append(os.path.join(directory, "%s_%s_alpha=0.10_gamma=0.990_epsilon=%.2f" % (algo, maze, epsilon)))
-    title = r"%s %s $\epsilon$" % (algo, maze)
-    plot_mean(files, num_exp, title, legend)
-
-
-plt.show()
+if __name__ == "__main__":
+    run_experiment(param_grid)
---
+++ experiments/e_greedy/plot.py
@@ -0,0 +1,39 @@
+import os
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from utilities.plot import plot_mean
+
+from experiments.e_greedy.experiment import alphas, directory, num_exp, gammas, epsilons
+
+mazes = ["GridWorldEasy", "GridWorldMedium"]
+algo = "QLearning"
+
+for maze in mazes:
+    files = []
+    legend = []
+    for alpha in alphas:
+        legend.append(r'alpha = %.2f' % alpha)
+        files.append(os.path.join(directory, "%s_%s_alpha=%.2f_gamma=0.990_epsilon=0.05" % (algo, maze, alpha)))
+    title = "%s %s alpha" % (algo, maze)
+    plot_mean(files, num_exp, title, legend)
+
+    files = []
+    legend = []
+    for gamma in gammas:
+        legend.append(r'gamma = %.3f' % gamma)
+        files.append(os.path.join(directory, "%s_%s_alpha=0.10_gamma=%.3f_epsilon=0.05" % (algo, maze, gamma)))
+    title = "%s %s gamma" % (algo, maze)
+    plot_mean(files, num_exp, title, legend)
+
+    files = []
+    legend = []
+    for epsilon in epsilons:
+        legend.append(r'$\epsilon$ = %.2f' % epsilon)
+        files.append(os.path.join(directory, "%s_%s_alpha=0.10_gamma=0.990_epsilon=%.2f" % (algo, maze, epsilon)))
+    title = r"%s %s $\epsilon$" % (algo, maze)
+    plot_mean(files, num_exp, title, legend)
+
+
+plt.show()
