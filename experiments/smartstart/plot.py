a001bf9d744c85d855be25ef3b7c22591c8d5a40
Bart Keulen <bart_keulen@hotmail.com>
2017-09-28 15:39:38 -0500

    Updated smartstart experiment, moved gcloud functionality back into develop

--- experiments/smartstart/plot.py
+++ experiments/smartstart/plot.py
@@ -3,26 +3,28 @@ import os
 import seaborn as sns
 
 from smartstart.utilities.plot import plot_summary, mean_reward_episode, \
-    mean_reward_std_episode, steps_episode
+    mean_reward_std_episode, steps_episode, show_plot
 
 sns.set_context("paper")
 
-directory = "/home/bartkeulen/repositories/smartstart/data/smartstart"
+directory = "/home/bartkeulen/repositories/smartstart/data/tmp/smartstart" \
+            "/GridWorldEasy"
 
 mazes = ["GridWorldEasy", "GridWorldMedium"]
 baselines = [31, 72]
-algos = ["QLearning", "SARSA", "SARSALamba"]
+algos = ["QLearning", "SARSA", "SARSALambda"]
 exploration_strategies = ["E-Greedy", "Count-Based", "Boltzmann", "None"]
 use_smart_start = [True, False]
 
-# mazes = ["GridWorldMedium"]
+mazes = ["GridWorldEasy"]
+baselines = [31]
 # algos = ["QLearning"]
-exploration_strategies = ["E-Greedy"]
-
+# exploration_strategies = ["E-Greedy"]
+#
 # colors = np.kron(np.asarray(sns.color_palette("husl", len(exploration_strategies))), np.ones((2, 1)))
 # colors = [tuple(row) for row in colors]
 # linestyles = ["solid", "dotted"] * len(exploration_strategies)
-colors = ["blue", "green"]
+# colors = ["blue", "green"]
 
 for maze, baseline in zip(mazes, baselines):
     for algo in algos:
@@ -39,14 +41,27 @@ for maze, baseline in zip(mazes, baselines):
                 files.append(os.path.join(directory, filename))
                 legendname += "%s" % exploration_strategy
                 legend.append(legendname)
+
         title = "%s_%s" % (algo, maze)
-        plot_summary(files, plot_type=steps_episode, ma_window=1, title=title + "_steps_episode", legend=legend,
-                     output_dir="/home/bartkeulen/IHMC/thesis/img/tmp", baseline=baseline)
 
-        plot_summary(files, plot_type=mean_reward_std_episode, ma_window=1, title=title + "_mean_reward_std_episode",
-                     legend=legend, output_dir="/home/bartkeulen/IHMC/thesis/img/tmp", format="png", baseline=1/baseline)
+        plot_summary(files,
+                     plot_type=steps_episode,
+                     ma_window=10,
+                     title=title + "_steps_episode",
+                     legend=legend,
+                     baseline=baseline)
 
-        plot_summary(files, plot_type=mean_reward_episode, ma_window=1, title=title + "_mean_reward_episode", legend=legend,
-                     output_dir="/home/bartkeulen/IHMC/thesis/img/tmp", baseline=1/baseline)
+        plot_summary(files,
+                     plot_type=mean_reward_std_episode,
+                     ma_window=10,
+                     title=title + "_mean_reward_std_episode",
+                     baseline=1/baseline)
 
-# plt.show()
+        plot_summary(files,
+                     plot_type=mean_reward_episode,
+                     ma_window=10,
+                     title=title + "_mean_reward_episode",
+                     legend=legend,
+                     baseline=1/baseline)
+
+show_plot()
---
+++ smartstart/gcloud/gcloud_config.py
@@ -0,0 +1,5 @@
+project_id = 'infra-rhino-169522'
+zone = 'us-east1-c'
+bucket_name = 'drl-data'
+image_name = 'drl-image'
+instances = ['drl-instance-2']
