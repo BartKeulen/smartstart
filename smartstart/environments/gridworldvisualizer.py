2a762d8d4f8f1daa761278748f107dd5678e2158
Bart Keulen <account+github@bartkeulen.com>
2017-11-22 14:25:44 +0100

    Changed visualizer to center in its quarter of the window instead of push against the top right corner

--- smartstart/environments/gridworldvisualizer.py
+++ smartstart/environments/gridworldvisualizer.py
@@ -122,7 +122,11 @@ class GridWorldVisualizer(Visualizer):
                 w = 2
                 if len(self.active_visualizers) > 2:
                     h = 2
-            size = (self.size[0] * w + self.spacing, self.size[1] * h + self.spacing)
+            # if w == 1 and h == 1:
+            #     size = (self.size[0] * w, self.size[1] * h)
+            # else:
+            #
+            size = (self.size[0] * w, self.size[1] * h)
             self.screen = pygame.display.set_mode(size, 0, 32)
             pygame.display.set_caption(self.name)
             self.clock = pygame.time.Clock()
@@ -143,38 +147,87 @@ class GridWorldVisualizer(Visualizer):
         # Fill background with black
         self.screen.fill(self.colors['background'])
 
-        # Render dividers
-        w, h = self.size
-        hor_divider = np.array([[0, h], [w*2 + self.spacing, h], [w*2 + self.spacing, h + self.spacing], [0, h + self.spacing]])
-        pygame.draw.polygon(self.screen, self.colors['wall'], hor_divider)
-        ver_divider = np.array([[w, 0], [w + self.spacing, 0], [w + self.spacing, h*2 + self.spacing], [w, h*2 + self.spacing]])
-        pygame.draw.polygon(self.screen, self.colors['wall'], ver_divider)
-
         # Render agent, value map, density map and console
         self.grid = self.env.get_grid()
         positions = [(1, 1), (0, 1), (1, 0), (0, 0)]
         if self.LIVE_AGENT in self.active_visualizers:
             pos = positions.pop()
+            self._render_grid(pos=pos)
+            self._render_borders(pos=pos)
             self._render_walls(pos=pos)
             self._render_elements("goal", "start", "agent", pos=pos)
             # self._render_map(grid, pos=positions.pop())
         if self.VALUE_FUNCTION in self.active_visualizers and value_map is not None:
             pos = positions.pop()
+            self._render_grid(pos=pos)
+            self._render_borders(pos=pos)
             self._render_walls(pos=pos)
             self._render_value_map(value_map, pos=pos)
             self._render_elements("goal", "start", pos=pos)
         if self.DENSITY in self.active_visualizers and density_map is not None:
             pos = positions.pop()
+            self._render_grid(pos=pos)
+            self._render_borders(pos=pos)
             self._render_walls(pos=pos)
             self._render_value_map(density_map, pos=pos)
             self._render_elements("goal", "start", pos=pos)
         if self.CONSOLE in self.active_visualizers:
-            self._render_console(pos=positions.pop(), message=message)
+            pos = positions.pop()
+            self._render_borders(pos=pos)
+            self._render_console(pos=pos, message=message)
 
         pygame.display.flip()
         self.clock.tick(self.fps)
 
         return True
+
+    def _render_grid(self, pos=(0, 0)):
+        grid_h, grid_w = self.grid.shape
+        w, h = self.size
+
+        offset_left, offset_top = self._get_offset(pos)
+        scale_w, scale_h, overshoot_w, overshoot_h = self._get_scale_overshoot()
+        border_left, border_top, border_right, border_bottom = self._get_borders(overshoot_w, overshoot_h)
+
+        color = (211, 211, 211, 255)
+        for y in range(grid_h):
+            start = np.array([border_left + offset_left, y * scale_h + border_top + offset_top])
+            end = np.array([w - border_left + offset_left, y * scale_h + border_top + offset_top])
+            pygame.draw.line(self.screen, color, start, end, 1)
+
+        for x in range(grid_w):
+            start = np.array([x * scale_w + offset_left + border_left, border_top + offset_top])
+            end = np.array([x * scale_w + offset_left + border_left, h - border_top + offset_top])
+            pygame.draw.line(self.screen, color, start, end, 1)
+
+    def _render_borders(self, pos=(0, 0)):
+        w, h = self.size
+        offset_left, offset_top = self._get_offset(pos)
+        scale_w, scale_h, overshoot_w, overshoot_h = self._get_scale_overshoot()
+        border_left, border_top, border_right, border_bottom = self._get_borders(overshoot_w, overshoot_h)
+
+        color = self.colors["wall"]
+        if border_left > 0:
+            vertices = np.array([[0 + offset_left, 0 + offset_top], [border_left + offset_left, 0 + offset_top],
+                                 [border_left + offset_left, h + offset_top], [0 + offset_left, h + offset_top]])
+            pygame.draw.polygon(self.screen, color, vertices)
+
+        if border_top > 0:
+            vertices = np.array([[0 + offset_left, 0 + offset_top], [w + offset_left, 0 + offset_top],
+                                 [w + offset_left, border_top + offset_top],
+                                 [0 + offset_left, border_top + offset_top]])
+            pygame.draw.polygon(self.screen, color, vertices)
+
+        if border_right > 0:
+            vertices = np.array([[w - border_right + offset_left, 0 + offset_top], [w + offset_left, 0 + offset_top],
+                                 [w + offset_left, h + offset_top], [w - border_right + offset_left, h + offset_top]])
+            pygame.draw.polygon(self.screen, color, vertices)
+
+        if border_bottom > 0:
+            vertices = np.array([[0 + offset_left, h - border_bottom + offset_top],
+                                 [w + offset_left, h - border_bottom + offset_top], [w + offset_left, h + offset_top],
+                                 [0 + offset_left, h + offset_top]])
+            pygame.draw.polygon(self.screen, color, vertices)
 
     def _render_elements(self, *args, pos=(0, 0)):
         """Render a single element
@@ -266,28 +319,6 @@ class GridWorldVisualizer(Visualizer):
 
                     pygame.draw.polygon(self.screen, color, vertices)
 
-        if border_left > 0:
-            vertices = np.array([[0 + offset_left, 0 + offset_top], [border_left + offset_left, 0 + offset_top],
-                                 [border_left + offset_left, h + offset_top], [0 + offset_left, h + offset_top]])
-            pygame.draw.polygon(self.screen, color, vertices)
-
-        if border_top > 0:
-            vertices = np.array([[0 + offset_left, 0 + offset_top], [w + offset_left, 0 + offset_top],
-                                 [w + offset_left, border_top + offset_top],
-                                 [0 + offset_left, border_top + offset_top]])
-            pygame.draw.polygon(self.screen, color, vertices)
-
-        if border_right > 0:
-            vertices = np.array([[w - border_right + offset_left, 0 + offset_top], [w + offset_left, 0 + offset_top],
-                                 [w + offset_left, h + offset_top], [w - border_right + offset_left, h + offset_top]])
-            pygame.draw.polygon(self.screen, color, vertices)
-
-        if border_bottom > 0:
-            vertices = np.array([[0 + offset_left, h - border_bottom + offset_top],
-                                 [w + offset_left, h - border_bottom + offset_top], [w + offset_left, h + offset_top],
-                                 [0 + offset_left, h + offset_top]])
-            pygame.draw.polygon(self.screen, color, vertices)
-
     def _render_value_map(self, value_map, pos=(0, 0)):
         """Render value map or density map
 
@@ -374,8 +405,8 @@ class GridWorldVisualizer(Visualizer):
             offset top
         """
         w, h = self.size
-        offset_left = pos[0] * (w + self.spacing)
-        offset_top = pos[1] * (h + self.spacing)
+        offset_left = pos[0] * w
+        offset_top = pos[1] * h
         return offset_left, offset_top
 
     def _get_scale_overshoot(self):
@@ -398,8 +429,8 @@ class GridWorldVisualizer(Visualizer):
         """
         grid_h, grid_w = self.grid.shape
         w, h = self.size
-        overshoot_w = w % grid_w
-        overshoot_h = h % grid_h
+        overshoot_w = (w - self.spacing) % grid_w
+        overshoot_h = (h - self.spacing) % grid_h
         scale_w = int((w - overshoot_w) / grid_w)
         scale_h = int((h - overshoot_h) / grid_h)
         return scale_w, scale_h, overshoot_w, overshoot_h
@@ -432,5 +463,10 @@ class GridWorldVisualizer(Visualizer):
 
         border_top = math.floor(overshoot_h / 2)
         border_bottom = overshoot_h - border_top
+
+        border_left += self.spacing/2
+        border_right += self.spacing/2
+        border_top += self.spacing/2
+        border_bottom += self.spacing/2
 
         return border_left, border_top, border_right, border_bottom
